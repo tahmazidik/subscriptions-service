78b9dc50ddd82296fcd4e0134b5ca72fc377a059
tahmazidik <tahmazidik@gmail.com>
2026-01-16 19:33:34 +0300

    feat: add minimal http server with health endpoint

---
+++ cmd/app/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	r := chi.NewRouter()
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ok"))
+	})
+
+	srv := &http.Server{
+		Addr:              ":" + port,
+		Handler:           r,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	log.Printf("listening on :%s", port)
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("server error: %v", err)
+	}
+
+	fmt.Println("bye")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tahmazidik/subscriptions-service
 
 go 1.25.5
+
+require github.com/go-chi/chi/v5 v5.2.4 // indirect
